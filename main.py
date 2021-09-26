5c93c0fd4a554f6b58c1d6313ea9fd7a96485da7
Laddge <dev.laddge@gmail.com>
2021-09-26 16:57:06 +0900

    [feat] apply auth

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+id_list = {
+    os.getenv('AUTH_ID'): os.getenv('AUTH_PW')
+}
+
+
+@auth.get_password
+def get_pw(id):
+    return id_list.get(id)
 
 
 @app.route('/')
+@auth.login_required
 def index():
     return 'hello, world'
 
