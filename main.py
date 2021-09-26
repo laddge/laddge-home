c5f4b8da4295454ad530981773a4a306d6a9c5a5
Laddge <dev.laddge@gmail.com>
2021-09-26 22:33:51 +0900

    [feat] add now weather

--- main.py
+++ main.py
@@ -26,16 +26,26 @@ def index():
 @app.route('/content.html')
 @auth.login_required
 def content():
-    res = requests.get('https://kids.yahoo.co.jp/today/')
-    soup = BeautifulSoup(res.text, 'html.parser')
+    dres = requests.get('https://kids.yahoo.co.jp/today/')
+    dsoup = BeautifulSoup(dres.content, 'html.parser')
     now = datetime.datetime.now(
         datetime.timezone(datetime.timedelta(hours=9))
     )
     d = {}
     d['date'] = '{}/{} {}'.format(now.month, now.day, now.strftime('%A')[:3])
-    d['dtltitle'] = soup.select_one('#dateDtl dt span').get_text()
-    d['dtl'] = soup.select_one('#dateDtl dd').get_text()
-    return render_template('content.html', d=d)
+    d['dtltitle'] = dsoup.select_one('#dateDtl dt span').text
+    d['dtl'] = dsoup.select_one('#dateDtl dd').text
+    wh = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; 404SC Build/MMB29K) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'}
+    wres = requests.get(
+        'https://weather.yahoo.co.jp/weather/jp/20/4810/20201.html',
+        headers=wh
+    )
+    wsoup = BeautifulSoup(wres.content, 'html.parser')
+    w = {}
+    w['icon'] = wsoup.select_one('.icon img').attrs['src']
+    w['temp'] = wsoup.select_one('.temperature td').text
+    w['prec'] = wsoup.select_one('.precipitation td').text
+    return render_template('content.html', d=d, w=w)
 
 
 @app.route('/test')
